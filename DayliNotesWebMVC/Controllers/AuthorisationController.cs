95d520c4c3b68b42a88b0f7934ee4f4af30de610
Makk1aud <1roman080283@mail.ru>
2023-09-25 17:41:00 +0600

    Make new structure of files

---
+++ DayliNotesWebMVC/Controllers/AuthorisationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayliNotesWebMVC.Controllers
+{
+    public class AuthorisationController : Controller
+    {
+        private readonly ILogger<AuthorisationController> _logger;
+
+        public AuthorisationController(ILogger<AuthorisationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
