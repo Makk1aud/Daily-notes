ff07f0139ab9c1b58298da4821714eee6a9ae5bc
Makk1aud <1roman080283@mail.ru>
2023-10-05 17:32:18 +0600

    New Html helper

--- DayliNotesWebMVC/Controllers/NoteController.cs
+++ DayliNotesWebMVC/Controllers/NoteController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DailyNotesWebApi;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol;
+using System.Text.Json.Nodes;
 
 namespace DayliNotesWebMVC.Controllers
 {
@@ -16,7 +21,28 @@ namespace DayliNotesWebMVC.Controllers
 
         public async Task<IActionResult> Notes(int clientId)
         {
-            return Ok(clientId.ToString());
+            HttpResponseMessage response = await _client.GetAsync(baseAddres + "/DailyNotes/GetNotesByClientId/" + clientId);
+            if(response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                //JObject result = JObject.Parse(data);
+
+                //var notesJS = result.Value<JArray>();
+                //List<Note> notes = notesJS.ToObject<List<Note>>();
+                //return View(notes);
+                var listNotes = JsonConvert.DeserializeObject(data);
+                List<Note> notes = ((JArray)listNotes).Select(x => new Note
+                {
+                    NoteId = (int)x["noteId"],
+                    ClientId = (int)x["clientId"],
+                    NoteTypeId = (int)x["noteTypeId"],
+                    NoteText = (string)x["noteText"],
+                    EditDate = (DateTime)x["editDate"],
+                    NoteTitle = (string)x["noteTitle"]
+                }).ToList();
+                return View(notes);
+            }
+            return View(null);
         }
     }
 }
---
+++ DayliNotesWebMVC/Helpers/BlockGenerateHelper.cs
@@ -0,0 +1,28 @@
+﻿using DailyNotesWebApi;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DayliNotesWebMVC.Helpers
+{
+    public static class BlockGenerateHelper
+    {
+        public static HtmlString BlockGenerate(this IHtmlHelper html, List<Note> listNotes)
+        {
+            int maxLength = 80;
+            string noteText = string.Empty; 
+            string result = string.Empty;
+            foreach (Note note in listNotes)
+            {
+                if (!string.IsNullOrEmpty(note.NoteText))
+                    noteText = note.NoteText.Length > maxLength ? note.NoteText.Substring(0, maxLength) : note.NoteText;
+                result += $"<div class=\"box\">" +
+                    $"<p>{note.NoteTitle}</p>" +
+                    $"<p>{noteText}...</p>" +
+                    $"<p>{note.EditDate}</p>" +
+                    $"</div>";
+                noteText = string.Empty;
+            }
+            return new HtmlString(result);
+        }
+    }
+}
