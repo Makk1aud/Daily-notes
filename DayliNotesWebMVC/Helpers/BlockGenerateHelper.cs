124675cca13018f3c8cac1117b8cba6add4c3219
Makk1aud <1roman080283@mail.ru>
2023-10-06 10:36:46 +0600

    Add modal window for add notes

--- DayliNotesWebMVC/Helpers/BlockGenerateHelper.cs
+++ DayliNotesWebMVC/Helpers/BlockGenerateHelper.cs
@@ -8,17 +8,17 @@ namespace DayliNotesWebMVC.Helpers
     {
         public static HtmlString BlockGenerate(this IHtmlHelper html, List<Note> listNotes)
         {
-            int maxLength = 80;
+            int maxLength = 60;
             string noteText = string.Empty; 
             string result = string.Empty;
             foreach (Note note in listNotes)
             {
                 if (!string.IsNullOrEmpty(note.NoteText))
                     noteText = note.NoteText.Length > maxLength ? note.NoteText.Substring(0, maxLength) : note.NoteText;
-                result += $"<div class=\"box\">" +
-                    $"<p>{note.NoteTitle}</p>" +
-                    $"<p>{noteText}...</p>" +
-                    $"<p>{note.EditDate}</p>" +
+                result += $"<div class=\"box\">\r\n    " +
+                    $"<p>{note.NoteTitle}</p>\r\n    " +
+                    $"<p>{noteText}</p>\r\n\r\n      " +
+                    $"<p >{note.EditDate}</p>\r\n\r\n  " +
                     $"</div>";
                 noteText = string.Empty;
             }
--- DayliNotesWebMVC/wwwroot/js/site.js
+++ DayliNotesWebMVC/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var modal = document.querySelector('.modal');
+
+// Get the button that opens the modal
+var btn = document.querySelector('.plus');
+
+// Get the close button element
+var close = document.querySelector('.close');
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on the close button, close the modal
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
